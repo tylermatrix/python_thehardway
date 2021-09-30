825f94af29e071553a71c85ba860b581d388c131
Tyler Matrix <Tylermaatrix@gmail.com>
2021-09-30 19:41:53 -0400

    re-added all the intro level python programs from printing to strings to loops and grabbing variables from a prompt

---
+++ lesson3_numbers.py
@@ -0,0 +1,26 @@
+
+#print statement to console
+print("I will now count my chickens: ")
+#prints hens, and return the calculation
+print("Hens", float(25 + 30/6))
+#added float to change int to float for accuracy
+print("Roosters", float(100-25 * 3 % 4))
+
+print("Now i will count my eggs:")
+#bedmas calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#does not calculate because it is a string
+print("Is it true that 3 + 2 < 5 -7?")
+#caluclates numbers
+print(3 + 2  < 5 -7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh that's why it's False")
+
+print("How about some more.")
+#this chunk returns booleans
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >=-2)
+print("is it less or equal?", 5 <= -2)
